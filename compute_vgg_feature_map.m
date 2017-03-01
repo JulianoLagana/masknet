006f59c511dee9c71e0d8148fcddc4fb0649e5b8
Juliano Lagana <julianotusi@gmail.com>
2017-03-01 14:56:43 +0100

    Makes compute_vgg_feature_map use less memory
    Makes compute_vgg_feature_map use the option 'conserveMemory' when calling vl_simplenn. Without this modification I was running into what seemed to be out of memory errors. After changing this the bug disappeared (changing it back makes the bug come back).

--- compute_vgg_feature_map.m
+++ compute_vgg_feature_map.m
@@ -1,14 +1,12 @@
 function [ feature_map ] = compute_vgg_feature_map( vgg_feature_computer, img )
 
-    net = vgg_feature_computer;
-
     % Preprocess image
     im_ = single(img) ; % note: 255 range
-    im_ = imresize(im_, net.meta.normalization.imageSize(1:2)) ;
-    im_ = im_ - net.meta.normalization.averageImage ;
+    im_ = imresize(im_, vgg_feature_computer.meta.normalization.imageSize(1:2)) ;
+    im_ = im_ - vgg_feature_computer.meta.normalization.averageImage ;
 
     % Compute feature maps
-    res = vl_simplenn(net, im_);
+    res = vl_simplenn(vgg_feature_computer, im_, [], [], 'conserveMemory', true);
     feature_map = res(end).x;
     
 
