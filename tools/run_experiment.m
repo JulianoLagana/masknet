1b13385c2f956912819ec1d5c5430468a258f28c
Juliano Lagana <julianotusi@gmail.com>
2017-03-02 22:51:23 +0100

    Creates a function and a script for organizing experiment runs

---
+++ tools/run_experiment.m
@@ -0,0 +1,29 @@
+function [ net, info ] = run_experiment( varargin )
+
+    % Default values for experiments
+    opts.imdb = 'full_imdb';
+    opts.modelName = [];
+    opts.learningRate = 2e-6;
+    opts.weightDecay = 0;
+    opts.batchSize = 50;
+    opts.numEpochs = 8;
+    opts.continue = false;
+    
+    % Override with user-defined values
+    opts = vl_argparse(opts,varargin);
+    
+    % Create experiment name
+    experimentName = [strrep(opts.imdb,'_','') '_' opts.modelName '_lr' num2str(opts.learningRate) '_wd' num2str(opts.weightDecay) '_batch' num2str(opts.batchSize)];
+    experimentName = strrep(experimentName,'.','p');
+    
+    % Run the experiment
+    [net, info] = cnn_masknet('imdbPath',fullfile(pwd,'data',[opts.imdb '.mat']), ...
+                    'expDir',fullfile(pwd, 'data', 'experiments', experimentName), ...
+                    'train',struct('learningRate', opts.learningRate, ...
+                                   'numEpochs',  opts.numEpochs, ...
+                                   'weightDecay', opts.weightDecay, ...
+                                   'batchSize', opts.batchSize, ...
+                                   'continue', opts.continue));    
+
+end
+
