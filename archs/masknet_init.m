f76d38045a9ed689c96855fd4fa38e54b45debd1
Juliano Lagana <julianotusi@gmail.com>
2017-03-14 13:41:09 +0100

    Intermediary development state...

--- archs/masknet_init.m
+++ archs/masknet_init.m
@@ -9,78 +9,52 @@ function [ net ] = masknet_init( varargin )
     % Override default with user-specified values
     opts = vl_argparse(opts, varargin) ;
 
-    % The first part is just a VGG network, with all the layers after the
+    
+    % The first part is pre-initialized VGG network, with all the layers after the
     % 14th removed
     net = load_vgg_feature_computer('data/imagenet-vgg-m.mat');
     net.meta = [];
+    net = dagnn.DagNN.fromSimpleNN(net, 'canonicalNames', true);
     
-    f = 1/100;
-    % Conv + ReLU layers
-    net.layers{end+1} = struct('type', 'conv',...
-                               'weights', {{f*randn(1,1,512,512, 'single'), zeros(1, 512, 'single')}} , ...
-                               'stride', 1, ...
-                               'pad', 0) ;
-    net.layers{end+1} = struct('type','relu');
+    % Convolution layer
+    convBlock6 = dagnn.Conv('size', [1 1 512 512], 'hasBias', true) ;
+    net.addLayer('conv6', convBlock6, {'x14'}, {'x15'}, {'conv6f', 'conv6b'}) ;
     
-    % Dropout layer
-    net.layers{end+1} = struct('type', 'dropout', ...
-                               'rate', opts.dropoutRate);  
-    
-    % Conv layer
-    net.layers{end+1} = struct('type','conv',...
-                               'weights', {{f*randn(13,13,512,512,'single'), zeros(1,512,'single')}}, ...
-                               'stride', 1, ...
-                               'pad', 0) ;                         
-                           
-    % Conv layer                           
-    net.layers{end+1} = struct('type','conv',...
-                               'weights', {{f*randn(1,1,512,56^2,'single'), zeros(1,56^2,'single')}}, ...
-                               'stride', 1, ...
-                               'pad', 0) ;  
-                           
-    % Reshape layer                           
-    net.layers{end+1} = struct('type','reshape',...
-                               'newDim', [56,56,1]);
-                           
-    % Bilinear upsampling layer
-    grid = single(create_meshgrid(opts.maskSize, opts.batchSize));
-    grid = gpuArray(grid);
-    net.layers{end+1} = struct('type','bilinear',...
-                               'grid',grid);     
-           
-    % Logistic log-loss layer                           
-    net.layers{end+1} = struct('type','loss','loss','logistic','class', []);                          
+    % ReLU layer
+    reluBlock6 = dagnn.ReLU() ;
+    net.addLayer('relu6', reluBlock6, {'x15'}, {'x16'}, {}) ;
 
-    % If requested, add the batch normalization layers
-    if opts.batchNormalization
-        net = insertBnorm(net, 1) ;
-        net = insertBnorm(net, 6) ;
-        net = insertBnorm(net, 11) ;
-        net = insertBnorm(net, 14) ;
-        net = insertBnorm(net, 17) ;
-        net = insertBnorm(net, 20) ;
-        net = insertBnorm(net, 25) ;
-    end
+    % Convolution layer
+    convBlock7 = dagnn.Conv('size', [13 13 512 512], 'hasBias', true) ;
+    net.addLayer('conv7', convBlock7, {'x16'}, {'x17'}, {'conv7f', 'conv7b'}) ;
     
-    % Tidy the net
-    net = vl_simplenn_tidy(net);
+    % Convolution layer
+    convBlock8 = dagnn.Conv('size', [1 1 512 56^2], 'hasBias', true) ;
+    net.addLayer('conv8', convBlock8, {'x17'}, {'x18'}, {'conv8f', 'conv8b'}) ;
+    
+    % Reshape layer
+    reshapeBlock = dagnn.Reshape('newDimensions',[56 56 1]);
+    net.addLayer('reshape',reshapeBlock, {'x18'},{'x19'},{});
+    
+    % Constant grid generator for the upsampling layer
+    grid = single(create_meshgrid(opts.maskSize, opts.batchSize));
+    constantGridBlock = dagnn.Constant('value', grid);
+    net.addLayer('constantGridGen',constantGridBlock,{},{'grid'});
+    
+    % Bilinear upsampling layer
+    bilinearBlock = dagnn.BilinearSampler();
+    net.addLayer('bilinear', bilinearBlock, {'x19', 'grid'}, {'x20'});
+    
+    % Logistic log-loss layer
+    logLossBlock = dagnn.Loss('loss','logistic');
+    net.addLayer('logloss', logLossBlock,{'x20','gtMask'},{'objective'});
+    
+    % Initialize parameters
+    net.initParams();
+    
     
     % Meta parameters
     net.meta.inputSize = [opts.maskSize(1) opts.maskSize(2) 3 opts.batchSize] ;
     net.meta.trainOpts = [];
 
-end
-
-
-function net = insertBnorm(net, l)
-
-    assert(isfield(net.layers{l}, 'weights'));
-    ndim = size(net.layers{l}.weights{1}, 4);
-    layer = struct('type', 'bnorm', ...
-                   'weights', {{ones(ndim, 1, 'single'), zeros(ndim, 1, 'single')}}, ...
-                   'learningRate', [1 1 0.05], ...
-                   'weightDecay', [0 0]) ;
-    net.layers{l}.biases = [] ;
-    net.layers = horzcat(net.layers(1:l), layer, net.layers(l+1:end)) ;
-    
 end
--- cnn_masknet.m
+++ cnn_masknet.m
@@ -1,85 +1,105 @@
 function [net, info] = cnn_masknet(varargin)
 %CNN_MASKNET  Trains the masknet.
 
-% Initializations
-opts.expDir = fullfile(pwd, 'data', 'exp') ;
-opts.dataDir =  fullfile(pwd, 'data');
-opts.imdbPath = fullfile(opts.dataDir, 'imdb.mat');
-opts.train = struct() ;
-opts.train.gpus = 1;
+    % Initializations
+    opts.expDir = fullfile(pwd, 'data', 'exp') ;
+    opts.dataDir =  fullfile(pwd, 'data');
+    opts.imdbPath = fullfile(opts.dataDir, 'imdb.mat');
+    opts.train = struct() ;
+    opts.train.gpus = 1;
 
-% Default network parameters
-opts.arch = 'deepmask';
-opts.net.batchSize = 50 ;
-opts.net.dropoutRate = 0.4;
+    % Default network parameters
+    opts.arch = 'deepmask';
+    opts.net.batchSize = 50 ;
+    opts.net.dropoutRate = 0.4;
 
-% Default training parameters
-opts.train.numEpochs = 3 ;
-opts.train.learningRate = 0.00001/5 ;
-opts.train.weightDecay = 0.00005 ;
-opts.train.momentum = 0.9 ;
-opts.train.continue = false;
+    % Default training parameters
+    opts.train.numEpochs = 3 ;
+    opts.train.learningRate = 0.00001/5 ;
+    opts.train.weightDecay = 0.00005 ;
+    opts.train.momentum = 0.9 ;
+    opts.train.continue = false;
 
-% Override any options with the user-defined values
-opts = vl_argparse(opts, varargin) ;
+    % Override any options with the user-defined values
+    opts = vl_argparse(opts, varargin) ;
 
-% Reset the GPU
-fprintf('%s: resetting GPU\n', mfilename) ;
-clear mex ;
-clear vl_tmove vl_imreadjpeg ;
-disp(gpuDevice(opts.train.gpus)) ;
+    % Reset the GPU
+    fprintf('%s: resetting GPU\n', mfilename) ;
+    clear mex ;
+    clear vl_tmove vl_imreadjpeg ;
+    disp(gpuDevice(opts.train.gpus)) ;
 
-% Load the chosen network architecture
-addpath archs;
-switch opts.arch
-    case 'deepmask'
-        net = deepmask_init(opts.net);
-    case 'deepmask_dropoutBefore'
-        net = deepmask_dropoutBefore_init(opts.net);
-    case 'deepmask_dropoutAfter'
-        net = deepmask_dropoutAfter_init(opts.net);
-    case 'deepmask_bNorm'
-        net = deepmask_bNorm_init(opts.net);
-end
-
-% Load the imdb file
-if exist(opts.imdbPath, 'file')
-  imdb = matfile(opts.imdbPath) ;
-else
-  error('imdb.mat file was not found')
-end
-
-% Separate data in training and validation samples (because of the way a
-% matfile object accesses data, they cannot be randomly sampled)
-temp = whos(imdb);
-nImages = temp(1).size(4);
-training_ratio = 0.7;
-validation_ratio = 0.15;
-set = zeros(nImages,1);
-for i = 1 : nImages
-    
-    % Training set
-    if i <= nImages*training_ratio
-        set(i) = 1;
-        
-    % Validation set
-    elseif i <= nImages*(training_ratio + validation_ratio)
-        set(i) = 2;
-        
-    % Test set
-    else
-        set(i) = 3;
+    % Load the chosen network architecture
+    isDag = false;
+    addpath archs;
+    switch opts.arch
+        case 'deepmask'
+            net = deepmask_init(opts.net);
+        case 'deepmask_dropoutBefore'
+            net = deepmask_dropoutBefore_init(opts.net);
+        case 'deepmask_dropoutAfter'
+            net = deepmask_dropoutAfter_init(opts.net);
+        case 'deepmask_bNorm'
+            net = deepmask_bNorm_init(opts.net);
+        case 'masknet'
+            net = masknet_init(opts.net);
+            isDag = true;
     end
-    
-end
 
-% Train
-[net, info] = cnn_train(net, imdb, @(x,y)getBatch(x,y), ...
-  'expDir', opts.expDir, ...
-  opts.train, ...
-  'batchSize', opts.net.batchSize, ...
-  'train', find(set == 1), ...
-  'val', find(set == 2)) ;
+    % Load the imdb file
+    if exist(opts.imdbPath, 'file')
+      imdb = matfile(opts.imdbPath) ;
+    else
+      error('imdb.mat file was not found')
+    end
+
+    % Separate data in training and validation samples (because of the way a
+    % matfile object accesses data, they cannot be randomly sampled)
+    temp = whos(imdb);
+    nImages = temp(1).size(4);
+    training_ratio = 0.7;
+    validation_ratio = 0.15;
+    set = zeros(nImages,1);
+    for i = 1 : nImages
+
+        % Training set
+        if i <= nImages*training_ratio
+            set(i) = 1;
+
+        % Validation set
+        elseif i <= nImages*(training_ratio + validation_ratio)
+            set(i) = 2;
+
+        % Test set
+        else
+            set(i) = 3;
+        end
+
+    end
+
+    % Choose batch function
+    if strcmp(opts.arch,'masknet')
+        batchFn = @(x,y) getBatchMasknet(opts.train,x,y);
+    else
+        batchFn = @(x,y)getBatch(x,y);
+    end
+
+    % Train
+    if isDag
+        [net, info] = cnn_train_dag(net, imdb, batchFn, ...
+          'expDir', opts.expDir, ...
+          opts.train, ...
+          'batchSize', opts.net.batchSize, ...
+          'train', find(set == 1), ...
+          'val', find(set == 2)) ;
+    else
+        [net, info] = cnn_train(net, imdb, batchFn, ...
+          'expDir', opts.expDir, ...
+          opts.train, ...
+          'batchSize', opts.net.batchSize, ...
+          'train', find(set == 1), ...
+          'val', find(set == 2)) ;    
+    end
 
 end
 
@@ -89,4 +109,20 @@ function [images, masks] = getBatch(imdb, batch)
 % --------------------------------------------------------------------
     images = single(imdb.imdb(:,:,:,batch));
     masks = single(imdb.masks(:,:,1,batch));
+end
+
+% --------------------------------------------------------------------
+function inputs = getBatchMasknet(opts,imdb, batch)
+% --------------------------------------------------------------------
+    if numel(opts.gpus) > 0
+        images = gpuArray(single(imdb.imdb(:,:,:,batch)));
+        partial_masks = gpuArray(single(imdb.partial_masks(:,:,1,batch)));
+        masks = gpuArray(single(imdb.masks(:,:,1,batch)));
+    else
+        images = single(imdb.imdb(:,:,:,batch));
+        partial_masks = single(imdb.partial_masks(:,:,1,batch));
+        masks = single(imdb.masks(:,:,1,batch));
+    end
+    
+    inputs = {'input',images,'gtMask',masks};
 end
