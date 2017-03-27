0bc1731ded9b9c8a7a417b9558acba4849af8088
Juliano Lagana <julianotusi@gmail.com>
2017-03-27 23:15:21 +0200

    Adds documentation for scores_to_segmented_image function

--- segmenter/scores_to_segmented_image.m
+++ segmenter/scores_to_segmented_image.m
@@ -1,4 +1,22 @@
 function [im_] = scores_to_segmented_image( im )
+% scores_to_segmented_image(im) transforms the output of FCN-8s to a single
+% channeled image, where the value of each pixel is the number of the class
+% to which it most probably belongs. This does so by going through all 
+% channels for each pixel and substituting all of them for the index of the 
+% highest value found.
+%
+% Inputs:
+% 
+%   - im : Output from FCN-8s. WxHxD image, where each pixel in channel 'k'
+%   represents the probability that that pixel belongs to class 'k'. W and
+%   H are specified in the 'meta.normalization.imageSize' field inside
+%   'net'.
+%   
+% Outputs:
+%
+%   - im_ : WxHx1 image, where the value of each pixel is the number of the
+%   class to which it most probably belongs.
+%
 
     imageSize = size(im);
     im_ = zeros(imageSize(1), imageSize(2));
